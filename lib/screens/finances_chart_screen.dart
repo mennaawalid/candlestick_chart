1a26979b700bd0b167a5dbc125ba64981ad74e5b
menna walid <mennawalidrahma@gmail.com>
2023-01-18 19:34:09 +0200

    created chart screen

---
+++ lib/screens/finances_chart_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FinacesChartScreen extends StatelessWidget {
+  const FinacesChartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'chart',
+        ),
+      ),
+    );
+  }
+}
